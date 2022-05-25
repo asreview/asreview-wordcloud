396edb742bbe0f811c8035f68b16217c1b3b7eff
Jonathan de Bruin <jonathandebruinos@gmail.com>
2022-05-25 18:25:53 +0200

    Happy lint

--- tests/test_wordcloud.py
+++ tests/test_wordcloud.py
@@ -2,9 +2,8 @@ from pathlib import Path
 
 from asreviewcontrib.wordcloud.entrypoint import word_cloud
 
+
 def test_wordcloud(tmpdir):
 
-	word_cloud(
-		"benchmark:van_de_schoot2017",
-		output_fp=Path(tmpdir, "demo.png")
-	)
+    word_cloud("benchmark:van_de_schoot2017",
+               output_fp=Path(tmpdir, "demo.png"))
